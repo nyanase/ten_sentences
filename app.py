9c7369064fd0ce5cb4f4c9a39706427e7b2e16d3
Nao <nao.yanase@wustl.edu>
2020-07-22 16:22:15 -0700

    added CORS

--- app.py
+++ app.py
@@ -1,8 +1,11 @@
 from flask import Flask, render_template, request, redirect, jsonify
 from flask_api import status
+from flask_cors import CORS
 from scripts.lsa import rank_lsa, get_content_from_url
 
 app = Flask(__name__)
+CORS(app)
+
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
@@ -26,8 +29,6 @@ def api_filter():
       return "Record not found", status.HTTP_400_BAD_REQUEST
     
     article_content = get_content_from_url(url)
-    print("AAAAAAAAAA")
-    print("article content", article_content)
     if article_content is False or article_content == "":
       return "Record not found", status.HTTP_400_BAD_REQUEST
     ranked = rank_lsa(article_content)
